0901b5d5cf08335817deb1f0c495f16e18b5a816
gwwwwt <811235355@qq.com>
2018-10-24 20:57:22 +0800

    First commit. Finish constant pool reading

---
+++ src/com/gwwwwt/test/Iphone6s.java
@@ -0,0 +1,11 @@
+package com.gwwwwt.test;
+
+public class Iphone6s {
+    int length = 138;
+    int widht = 67;
+    int height = 7;
+    int weight = 143;
+    int ram =2;
+    int rom = 16;
+    int pixel = 1200;
+}
