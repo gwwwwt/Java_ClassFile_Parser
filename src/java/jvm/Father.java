6d4931c65b47537432de97a517223cac4001f7da
gwwwwt <811235355@qq.com>
2018-10-25 14:27:36 +0800

    Add jvm demos

---
+++ src/java/jvm/Father.java
@@ -0,0 +1,18 @@
+package jvm;
+
+import net.sf.ehcache.pool.sizeof.AgentSizeOf;
+import net.sf.ehcache.pool.sizeof.SizeOf;
+
+public class Father {
+    private long l;
+    private byte b1;
+    private byte b2;
+
+
+    public static void main(String[] args) {
+        SizeOf sizeof = new AgentSizeOf();
+        Father father = new Father();
+
+        System.out.println("father's size: " + sizeof.sizeOf(father));
+    }
+}
